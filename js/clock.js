177689c41b7fd49a459214e06e92199bb58309df
LATIsi <p.kmj9911@gmail.com>
2025-11-23 00:55:37 +0900

    chore: 시간 년 월 일 요일 표기 오류 수정!

--- js/clock.js
+++ js/clock.js
@@ -9,7 +9,7 @@ function getClock(){
      const year  = String(date.getFullYear());
      const month  = String(date.getMonth()+1).padStart(2,"0");
      const day  = String(date.getDate()).padStart(2,"0");
-     const week  = weekarr[date.getDay()-1];
+     const week  = weekarr[date.getDay()];
      const hours = String(date.getHours()).padStart(2,"0");
      const minutes = String(date.getMinutes()).padStart(2,"0");
 
